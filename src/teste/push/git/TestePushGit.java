081b5a8aad8ded189beaec808c9aa27be51fd059
PC <PC@JEFF-PC>
2020-06-06 16:20:31 -0300

    teste commit

---
+++ src/teste/push/git/TestePushGit.java
@@ -0,0 +1,12 @@
+
+package teste.push.git;
+
+
+public class TestePushGit {
+
+
+    public static void main(String[] args) {
+        System.out.println("JUST A TEST PUSH IN NETBEANS!");
+    }
+    
+}
